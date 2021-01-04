8ce978d3819bf78b40ee7ac6a7bafd5fe4f73b12
Raunak <raunakb22@gmail.com>
2021-01-04 23:18:38 +0530

    Updated UI based version

--- index.js
+++ index.js
@@ -3,52 +3,59 @@
     1 = Paper
     2 = Scissors
 */
-const choices = ["Rock", "Paper",  "Scissors"];
+const rock = document.getElementById('rock'),
+    paper = document.getElementById('paper'),
+    scissors = document.getElementById('scissors');
 
-function computerPlay(){
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+const playerScore = document.getElementById('playerScore'),
+    compScore = document.getElementById('compScore');
+
+const round = document.getElementById('round'),
+    statusBar = document.getElementById('status-bar')
+
+let i = 0;
+
+rock.onclick = () => { playRound(0, computerPlay()) };
+paper.onclick = () => { playRound(1, computerPlay()) };
+scissors.onclick = () => { playRound(2, computerPlay()) };
+
+let player = 0, numRound = 1, comp = 0;
+
+function playRound(playerChoice, computerSelection) {
+    if (playerChoice === computerSelection) {
+        statusBar.textContent = 'It\'s a tie';
+    }
+    else if ((playerChoice === 0 && computerSelection === 2) || (playerChoice !== 0 && playerChoice > computerSelection)) {
+        statusBar.textContent = `You Won! ${choices[playerChoice]} beats ${choices[computerSelection]}`;
+        playerScore.textContent = ++player;
+    }
+    else {
+        statusBar.textContent = `You lose. ${choices[computerSelection]} beats ${choices[playerChoice]}`;
+        compScore.textContent = ++comp;
+    };
+    numRound++;
+    if (numRound <= 5)
+        round.textContent = numRound;
+    if (numRound === 6) {
+        let winner = (player > comp) ? 'You! Congrats!🎉' : (player === comp) ? 'A tie.' : 'Uh Oh! You lost. 🙁';
+        statusBar.textContent = 'The winner is...';
+        setTimeout(() => { statusBar.textContent = `${winner}` }, 2000);
+        setTimeout(reset, 5000);
+    }
+}
+
+function computerPlay() {
     return Math.ceil(Math.random() * (2));
 }
 
-function playRound(playerSelection, computerSelection){
-    let playerChoice = getChoice(playerSelection.toLowerCase());
-
-    if(playerChoice === -1)
-        return {hasWon: false, isTie: false, msg: 'Error!'};
-    if(playerChoice === computerSelection)
-        return {hasWon: false, isTie: false, msg:'It\'s a tie.'};
-    else if ((playerChoice === 0 && computerSelection === 2)||(playerChoice !== 0 && playerChoice > computerSelection))
-        return {hasWon: true, isTie: false, msg: `You Won! ${playerSelection} beats ${choices[computerSelection]}`};
-    else 
-        return {hasWon: false, isTie: false, msg:`You lose. ${choices[computerSelection]} beats ${playerSelection} `};
-}
-
-function getChoice(val){
-    return (val === "rock") ? 0 : (val === "paper") ? 1 : (val === "scissors") ? 2 : -1;
-}
-
-function game(){
-    let counter = 0, player = 0, computer = 0;
-
-    while(counter < 5){
-        let {hasWon, isTie, msg} = playRound(prompt("Enter your Choice!", "Rock"), computerPlay());
-        try{
-        if(!isTie){
-            if(hasWon)
-                player++;
-            else
-                computer++;
-        }
-        alert(msg);
-        }
-        catch(error){
-            alert(error);
-        }
-        counter++;
-    }
-
-    alert(`Your score is ${player} and computer score is ${computer}. There were ${5-player-computer} ties`);
-    (player > computer) ? alert('You won!') : (player === computer) ? alert('It was tie. Still a win!')
-    : alert('Oops, you lost. :( Better luck next time!');
-}
-
-game();
+function reset() {
+    player = 0;
+    numRound = 1;
+    comp = 1;
+    round.textContent = numRound;
+    playerScore.textContent = player;
+    compScore.textContent = comp;
+    statusBar.textContent = 'To start a new game, click another image!';
+};
